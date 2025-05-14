d2aecdba67c9def6e566d6844426691becda58e4
Mohit bhardwaj <82044623+mohitbhardwaji@users.noreply.github.com>
2025-05-14 19:22:13 +0530

    color and size fixes

--- src/pages/PDFGenerator.jsx
+++ src/pages/PDFGenerator.jsx
@@ -63,49 +63,60 @@ const PDFGenerator = ({
 
 
   const generatePDF = () => {
-    console.log("button clicked")
+    console.log("button clicked");
     const input = contentRef.current;
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('p', 'mm', 'a4');
-      const imgProps = pdf.getImageProperties(imgData);
+
+    // Set fixed dimensions for A5
+    input.style.width = "420px"; // A5 width in pixels
+    input.style.Height = "595px"; // A5 height in pixels
+
+    html2canvas(input, { scale: 2 }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a5"); // A5 page size
       const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-      pdf.save('download.pdf');
+      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+      pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+      pdf.save("download.pdf");
     });
   };
 
   return (
     <div className="p-4">
-    <div ref={contentRef} className="invoice-container border pb-4 bg-white text-black" style={{ borderColor: "#d1d5db" }}>
-      <div className="flex justify-center p-3" style={{ backgroundColor: "#94989a" }} > 
+    <div ref={contentRef} className="invoice-container border pb-1 bg-white text-black" style={{ borderColor: "#d1d5db" }}>
+      <div className="flex justify-center p-1" style={{ backgroundColor: "#94989a" }} > 
         <img src={logo} alt="Company Logo" className="h-15 w-auto" />
       </div>
 
-      <div className="p-2 text-[10px]">
-        <div className="mb-1 text-base text-center">
+      <div className="p-2 text-[8px]">
+        <div className=" text-base text-center">
           <p className="text-base font-semibold">{formType}</p>
         </div>
 
-        <div className="w-full text-right">
-          <p><strong>Serial No:</strong> {serialNumber}</p>
-          {formType !== 'Estimate' && (
-            <p><strong>GST No:</strong> 23AESPJ5532J1ZA</p>
-          )}
-          <p><strong>Sales Executive:</strong> {salesperson.name}</p>
-          <p><strong>Sales Contact:</strong> {salesperson.mobileNumber}</p>
+        <div className="flex justify-between text-[8px]">
+          {/* Left Section */}
+          <div>
+            <p><strong>Sales Executive:</strong> {salesperson.name}</p>
+            <p><strong>Sales Contact:</strong> {salesperson.mobileNumber}</p>
+          </div>
+
+          {/* Right Section */}
+          <div className="text-right">
+            <p><strong>Serial No:</strong> {serialNumber}</p>
+            {formType !== 'Estimate' && (
+              <p><strong>GST No:</strong> 23AESPJ5532J1ZA</p>
+            )}
+          </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 print:grid-cols-2 text-[10px]">
-          <p><strong>Date:</strong> {new Date(createdAt).toLocaleDateString()}</p>
-          <p><strong>Name:</strong> {customerName}</p>
-          <p><strong>Delivery Date:</strong> {new Date(deliveryDate).toLocaleDateString()}</p>
-          <p><strong>Phone:</strong> {customerPhone}</p>
-          <p><strong>Alternate Phone:</strong> {customerAltPhone}</p>
+
+        <div className="flex flex-wrap justify-between text-[8px]">
+          <p className="mr-2"><strong>Date:</strong> {new Date(createdAt).toLocaleDateString()}</p>
+          <p className="mr-2"><strong>Name:</strong> {customerName}</p>
+          <p className="mr-2"><strong>Delivery Date:</strong> {new Date(deliveryDate).toLocaleDateString()}</p>
+          <p className="mr-2"><strong>Phone:</strong> {customerPhone}</p>
+          <p className="mr-2"><strong>{customerAltPhone && ('Alternate Phone:')}</strong> {customerAltPhone}</p>
           {formType === 'Invoice' && (
-            <>
-              <p><strong>Invoice Number:</strong> {_id}</p>
-            </>
+            <p><strong>Invoice Number:</strong> {_id}</p>
           )}
         </div>
 
@@ -113,23 +124,23 @@ const PDFGenerator = ({
           <p><strong>Address:</strong> {customerAddress}</p>
         </div>
 
-        <table className="w-full border-collapse border mb-1 text-[10px]" >
+        <table className="w-full border-collapse  mb-1 text-[8px]" >
           <thead>
             <tr>
-              <th className="border px-2 py-1" >SNo.</th>
-              <th className="border px-2 py-1 text-left" >Item</th>
-              <th className="border px-2 py-1" >Price</th>
-              <th className="border px-2 py-1" >Qty</th>
-              <th className="border px-2 py-1" >Total</th>
+              <th className="border-l border-b border-t px-2 py-1" >SNo.</th>
+              <th className="border-l border-b border-t px-2 py-1 text-left" >Item</th>
+              <th className="border-l border-b border-t px-2 py-1" >Price</th>
+              <th className="border-l border-b border-t px-2 py-1" >Qty</th>
+              <th className="border-l border-b border-t border-r px-2 py-1" >Total</th>
             </tr>
           </thead>
           <tbody>
             {[...products, ...Array(10 - products.length)].map((item, i) => (
               <tr key={i}>
-                <td className={`${item ? 'border' : 'border-l'} px-2 py-1 text-center`} >
+                <td className={`${item ? 'border-l border-b py-1' : 'border-l'} px-2 text-center`} >
                   {item ? i + 1 : <div className="h-4" />}
                 </td>
-                <td className={`${item ? 'border' : ''} px-2 py-1 text-left`} >
+                <td className={`${item ? 'border-l border-b py-1' : ''} px-2 text-left`} >
                   {item?.productNumber}
                   {item?.s_number?.length > 0 && (
                     <div className="pl-4">
@@ -139,24 +150,24 @@ const PDFGenerator = ({
                     </div>
                   )}
                 </td>
-                <td className={`${item ? 'border' : ''} border px-2 py-1 text-center`} >
+                <td className={`${item ? 'py-1' : ''} border-l border-b px-2 text-center`} >
                   {item ? `₹${(item.customPrice - (item.customPrice * item.gst / 100)).toFixed(2)}` : <div className="h-4" />}
                 </td>
-                <td className={`${item ? 'border' : ''} border px-2 py-1 text-center`}  >
+                <td className={`${item ? ' py-1' : ''} border-l border-b px-2 text-center`}  >
                   {item?.quantity || <div className="h-4" />}
                 </td>
-                <td className={`${item ? 'border' : ''} border px-2 py-1 text-center`}  >
+                <td className={`${item ? 'py-1' : ''} border-l border-b border-r px-2 text-center`}  >
                   {item ? `₹${((item.customPrice - (item.customPrice * item.gst / 100)) * item.quantity).toFixed(2)}` : <div className="h-4" />}
                 </td>
               </tr>
             ))}
 
             <tr className="font-semibold">
-              <td colSpan="4" className="border px-2 py-1 text-right"  >Subtotal (Before GST)</td>
-              <td className="border px-2 text-center py-1"  >₹{calTotalwithoutAmount.toFixed(2)}</td>
+              <td colSpan="4" className="border px-2 text-right py-1"  >Subtotal (Before GST)</td>
+              <td className="border px-2 text-center py-1 "  >₹{calTotalwithoutAmount.toFixed(2)}</td>
             </tr>
             <tr className="font-semibold">
-              <td colSpan="4" className="border px-2 py-1 text-right"  >CGST</td>
+              <td colSpan="4" className="border px-2  text-right py-1"  >CGST</td>
               <td className="border px-2 text-center py-1"  >₹{(calTotalGST.toFixed(2) / 2)}</td>
             </tr>
             <tr className="font-semibold">
@@ -175,7 +186,7 @@ const PDFGenerator = ({
           <div className="grid grid-cols-1 md:grid-cols-2 print:grid-cols-2 gap-0">
   {/* Payment Details */}
   {formType !== 'Estimate' && (partialPayment || payment1?.amount || (partialPayment && payment2?.amount)) && (
-  <div className="text-[10px]">
+  <div className="text-[8px]">
     {partialPayment && (
       <>
         <p><strong>Advance Paid:</strong> ₹{advanceAmount.toFixed(2)}</p>
@@ -214,7 +225,7 @@ const PDFGenerator = ({
 
 
   {finance_id?.financerName && (
-    <div className="text-[10px]">
+    <div className="text-[8px]">
       <p><strong>Financer Name:</strong> {finance_id.financerName}</p>
       <p><strong>Downpayment:</strong> ₹{finance_id.downpayment}</p>
       <p><strong>EMI Tenure:</strong> {finance_id.emiTenure} months</p>
@@ -227,7 +238,7 @@ const PDFGenerator = ({
 </div>
 
 {/* Signature slightly above bottom */}
-<div className="absolute bottom-2 right-0 text-[10px] text-right pr-2">
+<div className="right-0 text-[8px] text-right mt-6 mr-2">
   Authorized Signature
 </div>  
 
